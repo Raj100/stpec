e85d98060e429888c6c635c2f6e244225a6712c7
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2025-02-06 07:01:48 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 10000,
   },
   server: {
     host: '0.0.0.0',
